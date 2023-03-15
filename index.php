b3cfd4eb197a82f68f97818daf11e6b8ecb4a632
mrTyagi <82006705+shubhamtyagi76@users.noreply.github.com>
2023-03-15 21:32:39 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qespy</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+</head>
+<body>
+ <ul>
+    <nav class="flex-div">
+        <div class="nav-left flex-div">
+           <img src="images/menu.png" class="menu-icon">
+            <img src="images/logo.png" class="logo">
+        </div>
+        <div class="nav-middle flex-div">
+        <div class="search-box flex-div">
+            <input type="text" placeholder="search">
+            <img src="images/search.png">
+        </div>
+        <img src="images/voice-search.png" class="mic-icon">
+        </div>
+        <div class="nav-right flex-div">
+            <a href="#"><img src="images/upload.png" class="upload-icon"><button UPLOAD ></button></a>
+            <div>
+                <div class="sub-menu-1">
+                    <ul>
+                       <li><a herf="">upload video <br><input type="file" id="file" accept="all"><label for="file"></label> </li>
+                       <!----- <a herf="">Go live</a>------>
+                    </ul>
+                    </div>
+            </div>
+            <img src="images/more.png">
+           <img src="images/notification.png">
+           <a href=""><img src="images/User.png" class="user-icon "></a>
+           
+        </div>
+    </nav>
+    <!------------sidebar------------->
+    <div class="sidebar">
+    <div class="shortcut-links">
+        <a href=""><img src="images/home.png"><p>Home</p></a>
+        <a href=""><img src="images/short.png"><p>Short</p></a>
+        <a href=""><img src="images/explore.png"><p>Explore</p></a>
+        <a href=""><img src="images/subscriprion.png"><p>subscriprion</p></a>
+        <a href=""><img src="images/library.png"><p>Library</p></a>
+        <a href=""><img src="images/history.png"><p>History</p></a>
+        <a href=""><img src="images/playlist.png"><p>playlist</p></a>
+        <a href=""><img src="images/messages.png"><p>Messages</p></a>
+        <a href=""><img src="images/show-more.png"><p>show more</p></a>
+            <hr>
+        </div>
+        <div class="subscribed-list">
+            <h3>SUBSCRIBED</h3>
+            <a href=""><img src="images/User.png"><p>user</p></a>
+            <hr>
+        </div>
+        <div>
+            <h4>Explore</h4>
+        </div>
+    </div>
+   
+<!----------main------------------->
+
+
+        
+<script src="script.js"></script>
+</ul>
+</body>
+</html>
