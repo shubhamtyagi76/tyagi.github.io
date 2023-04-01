9394f0512fee52ce1a0bae75bc8aa7e83283e716
mrTyagi <82006705+shubhamtyagi76@users.noreply.github.com>
2023-04-01 14:03:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container")
+var user = document.querySelector("user-icon");
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
-    
+    user.classlist.toggle("small-box")
 }
